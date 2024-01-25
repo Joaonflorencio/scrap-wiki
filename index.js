ef652676c3db27d5545ec7a8668b09a90c13edae
João Neto <jflorencioadv@gmail.com>
2024-01-25 22:04:23 +0100

    teste

--- index.js
+++ index.js
@@ -1,67 +1,77 @@
-// Requerir las dependencias
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cheerio = require('cheerio')
 
-// Inicializar Express
-const app = express();
-const port = 4007; // El puerto en el que se ejecutará el servidor
+const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap'
+const principalURL = 'https://es.wikipedia.org'
 
-// URL 
-const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap';
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(url)
+    const html = response.data
+    const $ = cheerio.load(html)
 
-app.get('/scrape', async (req, res) => {
-    try {
-        // Array para almacenar los datos scrapeados
-        const data = [];
-
-        // Obtener HTML de la categoría de músicos de rap
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
-
-        //  Recoger todos los enlaces dentro de #mw-pages
-        const links = [];
-        $('#mw-pages a').each((index, element) => {
-            const href = $(element).attr('href');
-            if (href) {
-                links.push(`https://es.wikipedia.org${href}`);
-            }
-        });
-
-        //  Recorrer cada enlace y realizar scraping de los datos
-        for (let link of links) {
-            const pageResponse = await axios.get(link);
-            const $$ = cheerio.load(pageResponse.data);
-
-            // Obtener el título
-            const title = $$('h1').text();
-
-            // Obtener todas las imágenes
-            const images = [];
-            $$('img').each((index, image) => {
-                images.push($$(image).attr('src'));
-            });
-
-            // Obtener todos los textos de los párrafos
-            const texts = [];
-            $$('p').each((index, paragraph) => {
-                texts.push($$(paragraph).text());
-            });
-
-            // Guardar los datos en el array
-            data.push({ title, images, texts });
-        }
-
-        //  Enviar los datos scrapeados
-        console.log(data); // O enviar con res.send(data);
-        res.send(data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error during scraping');
+    const links = []
+    $('#mw-pages a').each((index, element) => {
+      const link = $(element).attr('href')
+      links.push(link)
+    })
+    
+    const finalArray = [] 
+    
+    for(const link of links) {
+      const innerURL = `${principalURL}${link}`
+      const response = await axios.get(innerURL)
+      const innerPage = response.data
+      const $ = cheerio.load(innerPage)
+      
+      
+      const obj = {
+        imgs: [],
+        texts: []
+      }
+      
+      const title = $('h1').text()
+      obj.title = title
+     
+      $('img').each((index, element) => {
+        const img = $(element).attr('src')
+        obj.imgs.push(img)
+        console.log('imgs-->', img)
+      })
+      
+      $('p').each((index, element) => {
+          const text = $(element).text()
+          obj.texts.push(text)
+          console.log('texts-->', text)
+        })
+      finalArray.push(obj)
     }
-});
+        
+    res.send(`
+    <p>LINKS</p>
+    <ul>
+      ${links.map(data => `<li>${data}</li>`).join('')}
+    </ul>
+    <p>Cantantes</p>
+    <ul>
+      ${finalArray.map(data => `
+        <li>
+          <h2>${data.title}</h2>
+          <p>Imagenes: ${data.imgs.map(img => `<p>${img}</p>`).join('')}</p>
+          <p>Textos: ${data.texts.map(text => `<p>${text}</p>`).join('')}</p>
+        </li>`
+      )}
+    </ul>
+    `)
 
-// Iniciar el servidor
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+  } catch(error) {
+    console.log(error)
+  }
+})
+
+
+app.listen(3000, () => {
+  console.log('Express está escuchando en el puerto http://localhost:3000')
+})
